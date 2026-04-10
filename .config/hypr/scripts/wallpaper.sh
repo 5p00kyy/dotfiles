7ea5497263536259f89268011be08161c2f00f1b
pacey <partialabstraction@gmail.com>
2026-04-10 20:43:08 +1000

    refactor: split hyprland config into sourced files

--- .config/hypr/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wallpaper setup script for hyprpaper v0.8.x
-# Config file syntax changed, using IPC instead
-
-sleep 1  # Wait for hyprpaper to initialize
-
-hyprctl hyprpaper wallpaper "DP-1,/home/pacey/Pictures/blackhole.jpg"
-hyprctl hyprpaper wallpaper "DP-2,/home/pacey/Pictures/blackhole.jpg"
